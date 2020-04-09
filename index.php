acccd0ff434ac829257fe9b6037caa949b61ca49
tason69 <tason69@hotmail.com>
2020-04-09 12:01:04 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="FR-fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+        <title>espace</title>
+  </head>
+
+  <body>
+
+
+
+  
+
+
+
+
+
+
+    <script src="js/jquery-3.4.1.js"></script>
+    <script src="js/tooltip.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+    </body>
+    </html>
